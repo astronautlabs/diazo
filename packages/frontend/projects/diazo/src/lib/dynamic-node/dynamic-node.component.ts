43c3e674dbbd19835a8dd3a43d5524f0962300de
William Lahti <wilahti@gmail.com>
2023-06-18 02:44:37 -0700

    support customizable empty state for sidebar

--- packages/frontend/projects/diazo/src/lib/dynamic-node/dynamic-node.component.ts
+++ packages/frontend/projects/diazo/src/lib/dynamic-node/dynamic-node.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ComponentFactoryResolver, Injector } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Accessor, DiazoNode } from '@diazo/model';
 
 /**
